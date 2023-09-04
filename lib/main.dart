35589efdbd5011285682eaa8759f16bd9a739294
JubairSayeedLinas <jusali@143402#139822>
2023-09-04 18:20:00 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'home_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,33 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-     appBar: AppBar(
-       title: Text('Maps'),
-     ),
-
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(24.747149,90.420273)
-        ),
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-
-      ),
     );
   }
 }
